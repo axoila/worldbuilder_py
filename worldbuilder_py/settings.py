bfa3ff78683d97ed14019e52dc05d76cf2ab5c76
Ronja <axoila@gmx.de>
2016-01-07 20:09:42 +0100

    started renaming stuff

--- worldbuilder_py/settings.py
+++ worldbuilder_py/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls.apps.PollsConfig',
+    'worldbuilder.apps.PollsConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
