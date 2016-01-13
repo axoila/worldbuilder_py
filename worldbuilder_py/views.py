3b807ea8cc2588af6dce659fcb63a15218c86c81
Ronja <axoila@gmx.de>
2016-01-13 21:00:02 +0100

    css+html stuff

--- worldbuilder_py/views.py
+++ worldbuilder_py/views.py
@@ -24,7 +24,7 @@ def auth_view(request):
 
     if user is not None:
         auth.login(request, user)
-        return HttpResponseRedirect(reverse('loggedin'))
+        return HttpResponseRedirect(reverse('worldbuilder:index'))
     else:
         return HttpResponseRedirect(reverse('invalid'))
 
